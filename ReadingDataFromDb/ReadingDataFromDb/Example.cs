c4e68c3ba7361fcc42ac8cbd88ae41f22c130f2a
MarekApril <59579546+MarekApril@users.noreply.github.com>
2022-06-07 08:17:11 +0200

    Add files via upload

---
+++ ReadingDataFromDb/ReadingDataFromDb/Example.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingDataFromDb
+{
+    public class Example
+    {
+        private readonly string _connectionString = "Server=(localdb)\\studia;Database=Data;Trusted_Connection=True;";
+
+        protected  SqlConnection Connection { get; set; }
+
+        public Example()
+        {
+            Connection = new SqlConnection(_connectionString);
+        }
+
+        public string GetValues(int id)
+        {
+            using (SqlCommand cmd = Connection.CreateCommand())
+            {
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = @"SELECT [Name], [Surname] from [dbo].[Employees] Where [Id] = @Id;";
+                cmd.Parameters.AddWithValue(@"Id", id);
+                cmd.Connection.Open();
+
+                using (var reader = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    var surname = "";
+                    while (reader.Read())
+                    {
+                        surname = reader["Surname"].ToString();
+                    }
+
+                    return surname;
+                }
+            }
+        }
+    }
+}
---
+++ ReadingDataFromDb/ReadingDataFromDb/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace ReadingDataFromDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var example = new Example();
+            var result = example.GetValues(1);
+
+            Console.WriteLine(result);
+        }
+    }
+}
+
+
